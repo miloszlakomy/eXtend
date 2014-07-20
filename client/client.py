aa96a49c76505871409df6419fe9199a01ba558d
dextero <hukutizuviki+git@gmail.com>
2014-07-20 13:58:34 +0200

    Some client.py fixes

--- client/client.py
+++ client/client.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 
 import collections
+import errno
+import os
 import select
+import signal
 import socket
 import subprocess
 import sys
 from pymouse import PyMouse
 
+TCP_PORT = int(os.getenv('TCP_PORT') or 0x7e5d)
+UDP_PORT = int(os.getenv('UDP_PORT') or TCP_PORT)
+TMP_PREFIX = os.getenv('TMPDIR') or '/tmp'
+
+LOCK_FILE = TMP_PREFIX + '/.eXtend-client.lock'
+
 def get_screen_resolution():
     return PyMouse().screen_size()
+
+def lock():
+    if os.path.isfile(LOCK_FILE):
+        print('Another eXtend-client instance is running or previous one '
+              'crashed. Ensure there is no other client running or delete '
+              '%s file in case it crashed.' % LOCK_FILE)
+        sys.exit(1)
+
+    open(LOCK_FILE, 'w').close()
+
+def unlock():
+    os.remove(LOCK_FILE)
 
 class MessageBuffer(object):
     def __init__(self):
@@ -44,34 +65,43 @@ class EXtendClient(object):
         self.vnc_command = vnc_command
         self.vnc_process = None
 
+    def running(self):
+        return (self.connected
+                or (self.vnc_process and self.vnc_process.poll is not None))
+
     def run(self, listen_port):
         print('eXtend client daemon running')
         print('listening on UDP port %d' % listen_port)
         self.udp_socket.bind(('0.0.0.0', listen_port))
 
         while True:
-            fail_sockets = [ self.tcp_socket ] if self.connected else []
+            fail_sockets = [ self.tcp_socket ] if self.running() else []
             read_sockets = [ self.udp_socket ] + fail_sockets
 
-            ready, _, failed = select.select(read_sockets, [], fail_sockets)
+            try:
+                print('select')
+                ready, _, failed = select.select(read_sockets, [], fail_sockets)
 
-            if self.udp_socket in ready:
-                data, address = self.udp_socket.recvfrom(1024)
+                if self.udp_socket in ready:
+                    data, address = self.udp_socket.recvfrom(1024)
 
-                if not self.connected:
-                    self.connect((address[0], 6174))
+                    if not self.connected:
+                        self.connect((address[0], 6174))
 
-                self.udp_msg_buffer.update(data)
-                for msg in self.udp_msg_buffer:
-                    self.process_message(msg)
+                    self.udp_msg_buffer.update(data)
+                    for msg in self.udp_msg_buffer:
+                        self.process_udp_message(msg)
 
-            if self.tcp_socket in ready:
-                self.tcp_msg_buffer.update(self.tcp_socket.recv(1024))
-                for msg in self.tcp_msg_buffer:
-                    self.process_message(msg)
+                if self.tcp_socket in ready:
+                    self.tcp_msg_buffer.update(self.tcp_socket.recv(1024))
+                    for msg in self.tcp_msg_buffer:
+                        self.process_tcp_message(msg)
 
-            if self.tcp_socket in failed:
-                self.reset()
+                if self.tcp_socket in failed:
+                    self.reset()
+            except select.error as e:
+                if e[0] != errno.EINTR:
+                    raise
 
     def connect(self, address):
         print('connecting to %s:%d' % address)
@@ -89,17 +119,21 @@ class EXtendClient(object):
             self.vnc_process.terminate()
             self.vnc_process = None
 
-    def vnc_start(self, vnc_host, vnc_port):
+    def vnc_start(self, vnc_host, vnc_port, offset_x, offset_y):
         cmd = (self.vnc_command.replace('HOST', vnc_host)
                                .replace('PORT', vnc_port)).split()
+
+        self.display_offset_x = int(offset_x)
+        self.display_offset_y = int(offset_y)
 
         self.vnc_stop()
         self.vnc_process = subprocess.Popen(cmd)
 
     def set_cursor_pos(self, x, y):
-        PyMouse().move(x, y)
+        PyMouse().move(x - self.display_offset_x,
+                       y - self.display_offset_y)
 
-    def process_message(self, msg):
+    def process_message(self, msg, handlers):
         print('message: %s' % msg)
 
         stripped = msg.strip()
@@ -107,15 +141,30 @@ class EXtendClient(object):
             raise ValueError('empty message')
 
         words = stripped.split()
-        handler = {
-            'cursor': lambda x, y: self.set_cursor_pos(int(x), int(y)),
-            'vnc': lambda host, port: self.vnc_start(host, port)
-        }.get(words[0])
+        handler = handlers.get(words[0])
 
         if handler is not None:
             return handler(*words[1:])
         else:
             print('invalid message: %s' % msg)
 
-EXtendClient('vncviewer -viewonly HOST::PORT').run(6174)
+    def process_udp_message(self, msg):
+        return self.process_message(msg, {
+            'cursor': lambda x, y: self.set_cursor_pos(int(x), int(y)),
+        })
+
+    def process_tcp_message(self, msg):
+        return self.process_message(msg, {
+            'vnc': lambda *args: self.vnc_start(*args)
+        })
+
+def signal_handler(sig, stack_frame):
+    unlock()
+    sys.exit(1)
+
+for sig in [ signal.SIGTERM, signal.SIGINT, signal.SIGHUP ]:
+    signal.signal(sig, signal_handler)
+
+lock()
+EXtendClient('vncviewer -viewonly HOST::PORT').run(TCP_PORT)
 
