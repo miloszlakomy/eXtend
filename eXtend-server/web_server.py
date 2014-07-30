bc059d9495e628cb1d3ffc655f660a239f358595
dextero <hukutizuviki+git@gmail.com>
2014-07-31 00:16:23 +0200

    initial implementation

---
+++ eXtend-server/web_server.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+
+import shlex
+import select
+import socket
+import websocket
+import threading
+import message_buffer
+import collections
+import time
+import traceback
+
+LOGIN_URL = '/guacamole/login'
+LOGIN_USER = 'inz'
+LOGIN_PASS = 'inz'
+
+DEBUG = True
+
+def ws_print(text):
+    print('WebServer: ' + text)
+
+def ws_print_debug(text):
+    if DEBUG:
+        ws_print(text)
+
+class SocketClosed(Exception): pass
+class ClientReconnected(Exception): pass
+
+class WebClient(object):
+    def __init__(self, sock, addr):
+        self.sock = sock
+        self.addr = addr
+        self.msg_buffer = message_buffer.MessageBuffer()
+
+        self.id = None
+        self.resolution = None
+
+    def handle_reconnect(self, old_self):
+        ws_print('client %s reconnected (was %s)' % (self, old_self))
+
+        self.addr = old_self.addr
+        self.id = old_self.id
+        self.resolution = old_self.resolution
+
+        self.send('display %d %s' % (self.id, self._make_guacamole_url()))
+
+    def fileno(self):
+        """ makes it possible to use WebClient instances in select() calls """
+        return self.sock.fileno()
+
+    def update(self):
+        data = self.sock.recv(1024)
+
+        if data:
+            self.msg_buffer.update(data)
+
+            for msg in self.msg_buffer:
+                self._handle_message(msg)
+        else:
+            raise SocketClosed
+
+    def kill(self):
+        self.sock.close()
+        self.id = None
+
+    def send(self, commands):
+        if not isinstance(commands, list):
+            commands = [ commands ]
+
+        for resp in commands:
+            ws_print_debug('[%s] << %s' % (self, resp))
+            self.sock.send(resp + '\n')
+
+    
+
+    def _handle_message(self, msg):
+        ws_print_debug('[%s] >> %s' % (self, msg.strip()))
+
+        args = shlex.split(msg.strip())
+        handler = getattr(WebClient, "_MSG_" + args[0], None)
+
+        if handler:
+            try:
+                return handler(self, *args[1:])
+            except TypeError as e:
+                ws_print('ignoring invalid message: "%s" (%s)' % (msg, e[0]))
+        else:
+            ws_print('client %s self an unexpected message: %s' % (self, msg))
+
+    def _init_guacamole(self):
+        ws_print('init guacamole')
+        self.id = 0 # TODO
+
+    def _make_guacamole_url(self):
+        return '/guacamole/client.xhtml?id=c%2F' + str(self.id)
+
+    def __str__(self):
+        if self.id is not None:
+            return '<%s @ %s:%d>' % (self.id, self.addr[0], self.addr[1])
+        else:
+            return '<%s:%d>' % self.addr
+
+    # expected message flow:
+    #      CLIENT                     SERVER
+    #  connect <w> <h>  -->
+    #                       login <url> <login> <pass>
+    #                            <setup guacamole>
+    #                  <--      display <id> <url>
+    #  <refresh page>
+    #  reconnect <id>  -->
+    #                           display <id> <url>
+    def _MSG_connect(self, width, height):
+        if not self.id:
+            self.resolution = (width, height)
+            self._init_guacamole()
+        else:
+            ws_print('TODO: handle duplicate connect')
+
+        self.send([ 'login %s %s %s' % (LOGIN_URL, LOGIN_USER, LOGIN_PASS),
+                    'display %d %s' % (self.id, self._make_guacamole_url()) ])
+
+    def _MSG_reconnect(self, prev_id):
+        if self.id:
+            ws_print('received reconnect message, which is only valid on '
+                     'uninitialized clients')
+        else:
+            raise ClientReconnected(int(prev_id))
+
+class WebServerThread(threading.Thread):
+    class Zombie(object):
+        RECONNECT_TIMEOUT_S = 5
+
+        def __init__(self, client, time_last_seen):
+            self.client = client
+            self.time_last_seen = time_last_seen
+
+        def is_dead(self):
+            if time.time() - self.time_last_seen > RECONNECT_TIMEOUT_S:
+                self.client.kill()
+                # yep, it's dead
+                return True
+
+            return False
+
+        def __str__(self):
+            return 'Zombie: %s' % self.client
+
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+        self.local_addr = (host, port)
+        self.server_sock = None
+        self.clients = []
+
+        # guacamole web client won't work if there is no '?id=###' GET
+        # argument in the URL. there seems to be no way to set it without
+        # actually reloading the page, so clients have to reconnect to the
+        # server.
+        self.zombies = [] # disconnected clients that may yet reconnect
+
+    def _handle_new_client(self):
+        sock, addr = self.server_sock.accept()
+        ws_print('accepted new web client connection from %s:%d' % addr)
+        wrapped_sock = websocket.ServerWebsocket(sock)
+        self.clients.append(WebClient(wrapped_sock, addr))
+
+    def _find_zombie_by_id(self, id):
+        for z in self.zombies:
+            if z.client.id == id:
+                return z
+        return None
+
+    def _handle_client_reconnect(self, client, id):
+        zombie = self._find_zombie_by_id(id)
+        if not zombie:
+            raise ValueError('invalid reconnect request for client id = %s' % id)
+
+        client.handle_reconnect(zombie.client)
+        self.zombies.remove(zombie)
+
+    def _handle_client_disconnect(self, client):
+        self.zombies.append(WebServerThread.Zombie(client, time.time()))
+        self.clients.remove(client)
+        ws_print('client %s disconnected' % client)
+
+    def _handle_client_message(self, client):
+        try:
+            try:
+                client.update()
+            except ClientReconnected as e:
+                self._handle_client_reconnect(client, e[0])
+            except SocketClosed:
+                self._handle_client_disconnect(client)
+        except Exception as e:
+            ws_print(traceback.format_exc().strip())
+            ws_print('removing client because of error')
+            self.clients.remove(client)
+
+    def _clean_zombies(self):
+        self.zombies = [ z for z in self.zombies if not z.is_dead() ]
+
+    def _handle_sockets(self):
+        waitables = [ self.server_sock ] + self.clients
+        read, _, fail = select.select(waitables, [], waitables)
+        
+        for ready in read:
+            if self.server_sock is ready:
+                self._handle_new_client()
+            else:
+                self._handle_client_message(ready)
+
+        for failed in fail:
+            if self.server_sock is failed:
+                ws_print('server socket failed, thread shutting down')
+                return False
+            if failed in clients:
+                ws_print('socket failed for client: %s' % client)
+                clients.remove(failed)
+
+        return True
+
+    def run(self):
+        try:
+            self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.server_sock.bind(self.local_addr)
+
+            ws_print('starting websocket server on %s:%d'
+                     % (self.local_addr[0] or '0.0.0.0', self.local_addr[1]))
+            self.server_sock.listen(5)
+
+            try:
+                while self._handle_sockets():
+                    pass
+            finally:
+                self.server_sock.close()
+                self.clients = []
+                self.zombies = []
+        finally:
+            ws_print('server thread exiting')
+
+def start_server(host, port, backlog = 5):
+    WebServerThread(host, port).start()
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print('usage: %s <port>' % sys.argv[0])
+        sys.exit(1)
+
+    start_server('', int(sys.argv[1]))
+    while True:
+        time.sleep(1)
+
